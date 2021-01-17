32da6272c864ae8ca4f6707959a1879f700b5360
rin-m <s2010048@klis.tsukuba.ac.jp>
2021-01-17 16:44:38 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get '/users/:id', to: 'users#show', as: 'user'
+
+  get '/posts/new', to: 'posts#new'
+  post '/posts', to: 'posts#create'
+  post '/posts/:post_id/photos', to: 'photos#create', as: 'post_photos'
+
 end
